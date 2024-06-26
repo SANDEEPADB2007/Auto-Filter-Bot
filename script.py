5b9bc995cefe5aed1f17d41d938491f581905d00
SANDEEPADB2007 <mrupulbandara1976@gmail.com>
2024-06-26 16:05:02 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,11 +3,7 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
-
-Bot simply search for the files from provided channel according to given query and gives link to those files as buttons!
-
-So no need to add filters for your files or movies from now on!
+Hello I Am Songs Hub Sri Lanka Group Filter Bot.
 
 For more click <i>help</i></b>"""
 
@@ -31,12 +27,11 @@ Bot will automatically search for your file and give links to that!
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @SDBUB7
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
 
 """
